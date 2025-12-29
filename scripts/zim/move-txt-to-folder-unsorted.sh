60e125092c2fc4efccbbb86b31eb7defae88930b
Alexei Lupan <astenix@testitquickly.com>
2025-12-29 08:28:56 +0200

    + move-txt-to-folder-unsorted.sh

---
+++ scripts/zim/move-txt-to-folder-unsorted.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# = Глобальный файл с переменными =
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+source "$SCRIPT_DIR/../variables.sh"
+
+echo -e "\n\t>> Переместить файлы txt в блокнот Whisper под страницу $zim_folder_unsorted"
+
+# Зайти в каталог /input
+cd "$folder_input" || exit 1
+
+# Вывод и ошибки этой проверки глушатся.
+# Проверить, есть ли в каталоге /input хотя бы один *.txt.
+    # Если txt файлов нет → выдать ошибку
+    # Если txt файлы есть → перенести в каталог $zim_folder_unsorted все файлы txt
+if ls *.txt 1> /dev/null 2>&1; then
+    mkdir -p "$zim_folder_main/$zim_folder_unsorted"
+    mv *.txt "$zim_folder_main/$zim_folder_unsorted/"
+else
+    echo -e "\nОшибка!!!\n\tВ каталоге /input нет файлов *.txt\n"
+    exit 1
+fi
+
+echo -e "\nГотово"
