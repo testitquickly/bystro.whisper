a1f0bb1176f23280ecd70769b860fd1583bb970c
Alexei Lupan <astenix@testitquickly.com>
2026-04-27 13:46:31 +0300

    update-index-zim.sh — убрал открытие блокнота в Zim

--- update-index-zim.sh
+++ update-index-zim.sh
@@ -6,6 +6,6 @@
     # пересобрать содержимое страницы index
 ./scripts/zim/update-index.sh
 
-zim /home/astenix/Notebooks/Whisper/ index &
+#zim /home/astenix/Notebooks/Whisper/ index &
 
 echo -e "\nDone\n"
